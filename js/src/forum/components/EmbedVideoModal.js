081b3a475c35f4e4732b079afd92161ca78a46ee
William Di Cicco <nearata@protonmail.com>
2020-10-26 14:27:16 +0100

    Update to beta 14

--- js/src/forum/components/EmbedVideoModal.js
+++ js/src/forum/components/EmbedVideoModal.js
@@ -4,10 +4,12 @@ import Select from 'flarum/components/Select';
 import Switch from 'flarum/components/Switch';
 
 export default class EmbedVideoModal extends Modal {
-    init() {
-        this.videoUrl = m.prop('');
-        this.videoType = m.prop('normal');
-        this.isLive = m.prop(false);
+    oninit(vnode) {
+        super.oninit(vnode);
+
+        this.videoUrl = '';
+        this.videoType = 'normal';
+        this.isLive = false;
     }
 
     className() {
@@ -19,24 +21,24 @@ export default class EmbedVideoModal extends Modal {
     }
 
     content() {
-        return (
-            <div className="Modal-body">
-                <div className="Form">
-                    <div className="Form-group">
-                        <label>{app.translator.trans('nearata-embed-video.forum.modal.video_url_label')}</label>
-                        <input
-                            type="name"
-                            name="url"
-                            className="FormControl"
-                            disabled={this.loading}
-                            oninput={e => this.videoUrl(e.target.value)}
-                            autocomplete="off"
-                            placeholder={app.translator.trans('nearata-embed-video.forum.modal.video_url_placeholder')}
-                        />
-                    </div>
-                    <div className="Form-group">
-                        <label>{app.translator.trans('nearata-embed-video.forum.modal.video_type_label')}</label>
-                        {Select.component({
+        return [
+            m('.Modal-body', [
+                m('.Form', [
+                    m('.Form-group', [
+                        m('label', app.translator.trans('nearata-embed-video.forum.modal.video_url_label')),
+                        m('input', {
+                            type: 'name',
+                            name: 'url',
+                            class: 'FormControl',
+                            disabled: this.disabled,
+                            oninput: e => this.videoUrl = e.target.value,
+                            autocomplete: 'off',
+                            placeholder: app.translator.trans('nearata-embed-video.forum.modal.video_url_placeholder')
+                        })
+                    ]),
+                    m('.Form-group', [
+                        m('label', app.translator.trans('nearata-embed-video.forum.modal.video_type_label')),
+                        m(Select, {
                             options: {
                                 normal: app.translator.trans('nearata-embed-video.forum.modal.video_types.normal'),
                                 dash: app.translator.trans('nearata-embed-video.forum.modal.video_types.dash'),
@@ -45,28 +47,26 @@ export default class EmbedVideoModal extends Modal {
                                 shaka: app.translator.trans('nearata-embed-video.forum.modal.video_types.shaka'),
                                 webtorrent: app.translator.trans('nearata-embed-video.forum.modal.video_types.webtorrent')
                             },
-                            value: this.videoType(),
-                            onchange: this.videoType
-                        })}
-                    </div>
-                    <div className="Form-group">
-                        {Switch.component({
-                            children: app.translator.trans('nearata-embed-video.forum.modal.live_mode'),
-                            onchange: this.isLive,
-                            state: this.isLive()
-                        })}
-                    </div>
-                    <div className="Form-group">
-                        {Button.component({
+                            value: this.videoType,
+                            onchange: value => this.videoType = value
+                        })
+                    ]),
+                    m('.Form-group', [
+                        m(Switch, {
+                            onchange: value => this.isLive = value,
+                            state: this.isLive
+                        }, app.translator.trans('nearata-embed-video.forum.modal.live_mode'))
+                    ]),
+                    m('.Form-group', [
+                        m(Button, {
                             className: 'Button Button--primary Button--block',
                             type: 'submit',
-                            loading: this.loading,
-                            children: app.translator.trans('nearata-embed-video.forum.modal.submit_button'),
-                        })}
-                    </div>
-                </div>
-            </div>
-        )
+                            loading: this.loading
+                        }, app.translator.trans('nearata-embed-video.forum.modal.submit_button'))
+                    ])
+                ])
+            ])
+        ]
     }
 
     onsubmit(e) {
@@ -74,7 +74,7 @@ export default class EmbedVideoModal extends Modal {
 
         const playerUUID = window.crypto.getRandomValues(new Uint16Array(1))[0];
 
-        this.props.insertAtCursor(`[embed-video id="${playerUUID}" url="${this.videoUrl()}" type="${this.videoType()}" live="${this.isLive()}"]`);
+        this.attrs.editor.insertAtCursor(`[embed-video id="${playerUUID}" url="${this.videoUrl}" type="${this.videoType}" live="${this.isLive}"]`);
         this.hide()
     }
 }
--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -2,9 +2,8 @@ import app from 'flarum/app';
 import { extend } from 'flarum/extend';
 import Button from 'flarum/components/Button';
 import CommentPost from 'flarum/components/CommentPost';
-import DiscussionPage from 'flarum/components/DiscussionPage';
-import PostEdited from 'flarum/components/PostEdited';
 import TextEditor from 'flarum/components/TextEditor';
+import SuperTextarea from './utils/SuperTextarea';
 
 import EmbedVideoModal from './components/EmbedVideoModal';
 
@@ -23,57 +22,62 @@ app.initializers.add('nearata/flarum-ext-embed-video', () => {
     window.muxjs = muxjs;
     window.WebTorrent = WebTorrent;
 
+    extend(TextEditor.prototype, 'oncreate', function() {
+        const textArea = this.element.getElementsByTagName('textarea');
+        this.attrs.composer.editor = new SuperTextarea(textArea[0]);
+    });
+
     extend(TextEditor.prototype, 'controlItems', function(items) {
+        const editor = this.attrs.composer.editor;
         items.add(
             'nearataEmbedVideo',
             Button.component({
                 icon: 'fas fa-cat',
                 className: 'Button Button--icon',
-                onclick: () => app.modal.show(new EmbedVideoModal(this)),
-                title: app.translator.trans('nearata-embed-video.forum.button_tooltip_title')
-            })
+                onclick: () => app.modal.show(EmbedVideoModal, { editor: editor }),
+            }, app.translator.trans('nearata-embed-video.forum.button_tooltip_title'))
         )
     });
 
-    let playersList = [];
-    extend(CommentPost.prototype, 'config', () => {
-        const players = document.getElementsByClassName('dplayer-container');
+    const loadPlayers = videoPlayers => {
+        for (const p of videoPlayers) {
+            const videoUrl = p.dataset.url;
+            const videoType = p.dataset.type;
+            const liveMode = p.dataset.live;
 
-        for (let item of players) {
-            if (!playersList.includes(item.id)) {
-                playersList.push(item.id);
-                const id = item.id;
-                const videoUrl = item.dataset.url;
-                const videoType = item.dataset.type;
-                const liveMode = item.dataset.live;
-
-                new DPlayer({
-                    container: document.getElementById(id),
-                    live: liveMode === 'true' ? true : false,
-                    video: {
-                        url: videoUrl,
-                        type: videoType,
-                        customType: {
-                            dash: (video, player) => {
-                                window.dashjs.MediaPlayer().create().initialize(video, video.src, false);
-                            },
-                            shaka: (video, player) => {
-                                if (shaka.Player.isBrowserSupported()) {
-                                    new shaka.Player(video)
-                                    .load(video.src)
-                                    .then(() => {})
-                                    .catch(e => console.error(e));
-                                } else {
-                                    console.error('Error: Shaka is not supported.');
-                                }
+            new DPlayer({
+                container: p,
+                live: liveMode === 'true' ? true : false,
+                video: {
+                    url: videoUrl,
+                    type: videoType,
+                    customType: {
+                        dash: (video, player) => {
+                            window.dashjs.MediaPlayer().create().initialize(video, video.src, false);
+                        },
+                        shaka: (video, player) => {
+                            if (shaka.Player.isBrowserSupported()) {
+                                new shaka.Player(video)
+                                .load(video.src)
+                                .then(() => {})
+                                .catch(e => console.error(e));
+                            } else {
+                                console.error('Error: Shaka is not supported.');
                             }
                         }
                     }
-                });
-            }
+                }
+            });
         }
+    };
+
+    extend(CommentPost.prototype, 'oncreate', function () {
+        const videoPlayers = this.element.getElementsByClassName('dplayer-container');
+        loadPlayers(videoPlayers);
     });
 
-    extend(DiscussionPage.prototype, 'init', () => playersList = []);
-    extend(PostEdited.prototype, 'config', () => playersList = [])
+    extend(CommentPost.prototype, 'onupdate', function() {
+        const videoPlayers = this.element.getElementsByClassName('dplayer-container');
+        loadPlayers(videoPlayers);
+    });
 });
